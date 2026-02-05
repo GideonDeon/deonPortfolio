095f63c25405fcd3469adcd2950e2479cc2b4266
gideon <gideong351@gmail.com>
2026-02-05 14:09:21 +0100

    updated ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,7 @@ function ShadowEffect() {
     <>
       {isWinter ? (
         <div className="h-fit shadow-[0px_0px_20px_black] relative">
-          <Snowfall color="white" style={{zIndex:50}} />
+          <Snowfall color="white" style={{ zIndex: 50 }} />
           <Header />
           <Floaters />
           <Services />
@@ -72,8 +72,8 @@ function Header() {
     <div className="bg-[url(/images/future.png)] w-full h-82 lg:h-screen bg-center bg-cover bg-no-repeat bg-black/40 bg-blend-color">
       <PageNav />
       <div className="lg:grid grid-cols-2 gap-15">
-        <div className="lg:mt-30 xl:ml-35">
-          <span className="text-white text-2xl ml-2 sm:ml-20 lg:ml-10">
+        <div className="lg:mt-30 lg:ml-20 lg:pt-10 lg:grid grid-cols-[1fr_2fr] w-fit md:ml-15">
+          <span className="text-white text-2xl ml-4 sm:ml-19 lg:ml-0 xl:ml-20 lg:pt-5 w-15">
             <a href="https://github.com/GideonDeon" className="ml-0.5">
               <FontAwesomeIcon
                 icon="fa-brands fa-github"
@@ -105,35 +105,39 @@ function Header() {
               />
             </a>
           </span>
-          <p
-            className="text-white text-[18px] font-mono uppercase ml-8 mt-5 sm:ml-25 lg:text-2xl 
-                        lg:ml-20"
-          >
-            I am a Full-
-            <span className=" inline-block animate-blink">stack</span> develope
-            <span className="inline-block animate-blinky">r</span>
-          </p>
-          <p
-            className="text-white text-[14px] font-mono font-light ml-8 pr-5 mt-2 text-justify 
-                        sm:w-120 sm:ml-25 lg:text-[16px] lg:ml-20 lg:w-100"
-          >
-            Hello! I'm Gideon, a professional Full-Stack Developer with a strong
-            frontend focus, experienced in building end-to-end web applications.
-          </p>
-          <div className="pt-3 w-fit ml-8 sm:ml-25 lg:ml-20 lg:mt-2">
-            <a
-              href="#contact"
-              className="text-gray-300 mr-5 p-2 pt-1 border-2 rounded-[10px] border-[#46a293] hover:bg-gray-400 hover:text-white"
+          <span className="block">
+            <p
+              className="text-white text-[18px] font-mono uppercase ml-6 mt-5 sm:ml-21 lg:text-2xl 
+                        lg:ml-0"
             >
-              Hire me
-            </a>
-            <a
-              href="/cv/gideon_oluwanifemi.pdf"
-              className="text-gray-300 mr-5 p-2 pt-1 border-2 rounded-[10px] border-[#46a293] hover:bg-gray-400 hover:text-white"
+              I am a Full-
+              <span className=" inline-block animate-blink">stack</span>{" "}
+              develope
+              <span className="inline-block animate-blinky">r</span>
+            </p>
+            <p
+              className="text-white text-[14px] font-mono font-light ml-6 pr-5 mt-2 text-justify 
+                        sm:w-120 sm:ml-21 lg:text-[16px] lg:ml-0 lg:w-100"
             >
-              Download CV
-            </a>
-          </div>
+              Hello! I'm Gideon, a professional Full-Stack Developer with a
+              strong frontend focus, experienced in building end-to-end web
+              applications.
+            </p>
+            <div className="pt-3 w-fit ml-6 sm:ml-21 lg:ml-0 lg:mt-2">
+              <a
+                href="#contact"
+                className="text-gray-300 mr-5 p-2 pt-1 border-2 rounded-[10px] border-[#46a293] hover:bg-gray-400 hover:text-white"
+              >
+                Hire me
+              </a>
+              <a
+                href="/cv/gideon_oluwanifemi.pdf"
+                className="text-gray-300 mr-5 p-2 pt-1 border-2 rounded-[10px] border-[#46a293] hover:bg-gray-400 hover:text-white"
+              >
+                Download CV
+              </a>
+            </div>
+          </span>
         </div>
         <div className="hidden lg:block">
           <img
